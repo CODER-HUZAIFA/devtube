07ce8ec119f4a8180d94284a69a6557845394ea4
CODER-HUZAIFA <ansarihuzaifa9825@gmail.com>
2024-08-30 23:15:23 +0530

    Home Pae almost done

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,6 @@
-var a = 10
+let menu = document.querySelector(".top")
+let section = document.querySelector(".leftSection")
+
+menu.addEventListener("click", () => {
+    section.style.width = "200px"
+})
